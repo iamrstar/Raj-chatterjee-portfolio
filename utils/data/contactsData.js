f17b7af9834d7ac8c92aa99d82db88eedec834d0
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2024-11-08 00:22:15 +0530

    first-commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'rajchatterji20@gmail.com',
+    phone: '+919835514242',
+    address: 'Saraidhela Dhanbad, Jharkhand, India - 828127 ',
+    github: 'https://github.com/iamrstar',
+    facebook: 'https://www.facebook.com/rajchatterji/',
+    linkedIn: 'https://www.linkedin.com/in/raj-chatterjee-a936a1211/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/',
+    devUsername: "iamrstar"
+}
