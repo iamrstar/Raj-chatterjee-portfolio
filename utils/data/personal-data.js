f59187b2a3302bcad5146c1561cccb468c7af32a
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-07-11 16:57:49 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,10 +2,6 @@ export const personalData = {
   name: "Raj Chatterjee ",
   profile: "/profile.png",
   designation: "Software Developer",
-export const personalData = {
-  name: "Raj Chatterjee ",
-  profile: "/profile.png",
-  designation: "Software Developer",
 description: "Hi, I’m Raj Chatterjee — a passionate and enthusiastic programmer with a strong self-learning mindset. I enjoy exploring new technologies and solving real-world problems through code. My core strength lies in JavaScript, and I specialize in full-stack web development, with a keen interest in building user-friendly and scalable web applications. I'm always eager to learn, grow, and contribute to impactful projects. I’m open to exciting opportunities that align with my skills, especially in web development, problem-solving, and innovative tech.",
   email: 'rajchatterji20@gmail.com',
   phone: '+919835514242',
@@ -16,18 +12,6 @@ description: "Hi, I’m Raj Chatterjee — a passionate and enthusiastic program
   twitter: 'https://twitter.com/rajchatterjee',
   stackOverflow: 'https://stackoverflow.com',
   leetcode: "https://leetcode.com/u/iamrstar/",
-  devUsername: "iamrstar",
-  resume: "https://drive.google.com/file/d/1OopRMC99bYWgFW_41kLmh_njgN_buEGJ/view?usp=sharing"
-}
-  email: 'rajchatterji20@gmail.com',
-  phone: '+919835514242',
-  address: ' Dhanbad, Jharkhand, India - 828127 ',
-  github: 'https://github.com/iamrstar',
-  facebook: 'https://www.facebook.com/rajchatterji/',
-  linkedIn: 'https://www.linkedin.com/in/raj-chatterjee-a936a1211/',
-  twitter: 'https://twitter.com/rajchatterjee',
-  // stackOverflow: 'https://stackoverflow.com',
-  // leetcode: "https://leetcode.com/u/iamrstar/",
   devUsername: "iamrstar",
   resume: "https://drive.google.com/file/d/1OopRMC99bYWgFW_41kLmh_njgN_buEGJ/view?usp=sharing"
 }
