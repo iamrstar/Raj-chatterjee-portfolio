96aede4ef280e0a45ab4d6f2686138a5325e10a8
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-12-04 12:26:25 +0530

    Update Twitter handle in contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -5,7 +5,7 @@ export const contactsData = {
     github: 'https://github.com/iamrstar',
     facebook: 'https://www.facebook.com/rajchatterji/',
     linkedIn: 'https://www.linkedin.com/in/raj-chatterjee-a936a1211/',
-    twitter: 'https://twitter.com/said7388',
+    twitter: 'https://twitter.com/RajChatterjee51',
     stackOverflow: 'https://stackoverflow.com/users/',
     devUsername: "iamrstar"
 }
