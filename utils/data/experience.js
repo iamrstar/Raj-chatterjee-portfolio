aa793084d5e65be53874bd8a7c58bf8db44ea0a8
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-12-04 12:19:28 +0530

    Update experience data with new job entries

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,16 +1,17 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Full Stack Developer',
-    company: "A1 NETWORK And IT SOLUTION Ltd.",
-    duration: "(FEB 2025 - Present)"
+    title: "Software Developer",
+    company: "Engineers Parcel",
+    duration: "(AUG 2025 - PRESENT)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "(freelance)",
-    duration: "(Jun 2022 - Jan 2023)"
+    title: 'Full Stack Developer',
+    company: "A1 NETWORK And IT SOLUTION Ltd.",
+    duration: "(FEB 2025 - JUL 2025)"
   },
+  
   {
     id: 3,
     title: "Self Employed",
@@ -18,3 +19,4 @@ export const experiences = [
     duration: "(Jan 2021 - Present)"
   }
 ]
+
