e1cc0f298eb49bccab9e8f9f9dcc13c66a64654f
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-12-04 12:35:49 +0530

    Update designation in personal data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Raj Chatterjee ",
   profile: "/profile.png",
-  designation: "Software Developer",
+  designation: "Software Developer | Data Engineer Enthusiast",
 description: "Hi, I’m Raj Chatterjee — a passionate and enthusiastic programmer with a strong self-learning mindset. I enjoy exploring new technologies and solving real-world problems through code. My core strength lies in JavaScript, and I specialize in full-stack web development, with a keen interest in building user-friendly and scalable web applications. I'm always eager to learn, grow, and contribute to impactful projects. I’m open to exciting opportunities that align with my skills, especially in web development, problem-solving, and innovative tech.",
   email: 'rajchatterji20@gmail.com',
   phone: '+919835514242',
